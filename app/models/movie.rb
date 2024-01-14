0247a6c38217da885285e1c294f8d5ace2df7d9a
Jafer Daltro <jafer.daltro@gmail.com>
2024-01-14 15:56:12 -0300

    Add validations to movie.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   has_many :ratings
+
+  validates :title, presence: true
+  validates :release_date, presence: true
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:release_date) }
   it { is_expected.to have_many :ratings }
 end
