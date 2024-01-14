2118e9ef8c36c5ed9250d9809d0603456f927f50
Jafer Daltro <jafer.daltro@gmail.com>
2024-01-14 14:35:54 -0300

    Fix service test.

--- spec/services/movie_rating_service_spec.rb
+++ spec/services/movie_rating_service_spec.rb
@@ -4,12 +4,15 @@ RSpec.describe MovieRatingService do
   describe '.calculate_average_rating' do
     it 'returns 0 for a movie with no ratings' do
       movie = build(:movie)
-      expect(described_class.calculate_average_rating(movie)).to eq(0)
+      expect(described_class.calculate_rating(movie)).to eq(0)
     end
 
     it 'calculates the average rating for a movie with ratings' do
-      movie = build(:movie_with_ratings, ratings_count: 3)
-      expect(described_class.calculate_average_rating(movie)).to eq(3.0)
+      movie = build(:movie, :with_ratings)
+      movie.ratings << build(:rating, grade: 3)
+      movie.save!
+      
+      expect(described_class.calculate_rating(movie)).to eq(3.0)
     end
   end
 end
