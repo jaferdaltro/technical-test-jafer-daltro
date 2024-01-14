e7795757264f4464a843970cc160368cf9797fdc
Jafer Daltro <jafer.daltro@gmail.com>
2024-01-14 12:04:46 -0300

    Refactored code
    Make some improvements in all end points in order to be more SOLID.
    Add service to be more DRY.

--- app/api/movies/api.rb
+++ app/api/movies/api.rb
@@ -10,11 +10,7 @@ module Movies
         movies = Movie.includes(:ratings).order(:release_date).reverse_order
 
         movies.each do |movie|
-          if movie.ratings.count == 0
-            movie.rating = 0
-          else
-            movie.rating = movie.ratings.map(&:grade).sum / movie.ratings.map(&:grade).count
-          end
+          movie.rating = MovieRatingService.calculate_rating(movie)
         end
       end
 
@@ -24,13 +20,11 @@ module Movies
       end
 
       get '/search' do
-        movies = Movie.where("title ILIKE '%#{params[:title]}%'")
+        movies = Movie.where('title ILIKE ?', "%#{params[:title]}%")
 
-        if movies.any?
-          movies.first
-        else
-          error! "nothing for this search", :internal_server_error
-        end
+        error!("nothing for this search", :not_found) if movies.empty?
+
+        movies.first
       end
 
       desc 'Show information about a particular movie'
@@ -41,37 +35,26 @@ module Movies
       get '/:id' do
         movie = Movie.find_by_id(params[:id])
 
-        if movie
-          if movie.ratings.count == 0
-            movie.rating = 0
-          else
-            movie.rating = movie.ratings.map(&:grade).sum / movie.ratings.map(&:grade).count
-          end
-          movie
-        else
-          error! "not found", :internal_server_error
-        end
+        error!('Movie not fount', :not_found) unless movie
+
+        movie_rating = MovieRatingService.calculate_rating(movie)
+        movie
       end
 
       desc 'Create a movie.'
       params do
-        requires :title, type: String, desc: 'Movie title.'
-        requires :release_date, type: Date, desc: 'Movie release date'
-        optional :runtime, type: String, desc: 'Movie runtime'
-        optional :genre, type: String, desc: 'Movie genre'
-        optional :parental_rating, type: String, desc: 'Movie'
-        optional :plot, type: String, desc: 'Movie'
+        requires :movie, type: Hash do
+          requires :title, type: String, desc: 'Movie title.'
+          requires :release_date, type: Date, desc: 'Movie release date'
+          optional :runtime, type: String, desc: 'Movie runtime'
+          optional :genre, type: String, desc: 'Movie genre'
+          optional :parental_rating, type: String, desc: 'Movie'
+          optional :plot, type: String, desc: 'Movie'
+        end
       end
 
       post do
-        Movie.create!({
-          title: params[:title],
-          release_date: params[:release_date],
-          runtime: params[:runtime],
-          genre: params[:genre],
-          parental_rating: params[:parental_rating],
-          plot: params[:plot]
-        })
+        Movie.create!(params[:movie])
       end
 
       desc 'Delete a movie.'
@@ -79,7 +62,12 @@ module Movies
         requires :id, type: String, desc: 'movie ID.'
       end
       delete ':id' do
-        Movie.find(params[:id]).destroy
+        movie = Movie.find(params[:id])
+
+        error!('Movie not found', :not_found) unless movie
+
+        movie.destroy
+        { message: 'Movie deleted successfully' }
       end
     end
   end
